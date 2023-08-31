f5b7dd408cb2ec33c83824c231ed1ad0e8fa80dc
poseidon200500 <poseidon200500@yandex.ru>
2023-08-31 15:49:53 +0300

    поиск и удаление нулевых рядов (не закончено)

--- tetris_logic/logic.go
+++ tetris_logic/logic.go
@@ -13,7 +13,36 @@ func next_gen(field [20][10]int, figure figure_struct.Figure) [20][10]int {
 
 /*
 Функция проходит по слоям марицы (по горизонтали) и если встречает заполненный слой уничтожает его
-*/
-func del_ful_row(field [20][10]int) {
 
+На вход подаётс матрица с учётом, что координата (0,0) совпадает с field[0][0], возможно потребуется
+перевернуть матрицу
+*/
+func find_ful_row(field [20][10]int) {
+	nul_count := 0
+	null_row := true
+	for j := 0; j < 20; j++ {
+		for i := 0; i < 10; i++ {
+			if field[i][j] != 0 {
+				null_row = false
+				break
+			}
+		}
+		if null_row {
+			nul_count++
+		} else {
+			if nul_count != 0 {
+				field = del_ful_row(field, j, nul_count) //перемещает ряды начиная с j и до конца
+				j -= nul_count
+				nul_count = 0
+			}
+		}
+	}
+}
+
+func del_ful_row(field [20][10]int, row int, count int) [20][10]int {
+	for j := row; j < 20; j++ {
+		for i := 0; i < 10; i++ {
+
+		}
+	}
 }
